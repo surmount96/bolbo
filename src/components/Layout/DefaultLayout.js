17fbf09368c240ba2b9ec4f88bf4900cd5f0ceb8
Ayodeji Ayanwoye <ayanwoye74@gmail.com>
2023-07-28 16:39:05 +0100

    max width limit

--- src/components/Layout/DefaultLayout.js
+++ src/components/Layout/DefaultLayout.js
@@ -3,11 +3,11 @@ import Navbar from "../Inc/Navbar";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className="max-w-[1600px] mx-auto">
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
